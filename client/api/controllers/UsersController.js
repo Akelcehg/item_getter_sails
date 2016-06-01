abf05725f8722a3428989987e6a66117623a7880
akelcheg <akelcheg@gmail.com>
2016-06-01 14:17:42 +0300

    init

---
+++ client/api/controllers/UsersController.js
@@ -0,0 +1,15 @@
+/**
+ * UsersController
+ *
+ * @description :: Server-side logic for managing users
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+
+  hi: function (req, res) {
+    return res.send("Hi there!");
+  }
+
+};
+
