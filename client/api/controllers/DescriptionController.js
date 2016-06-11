690fbcd1a05a82e01f5bccf4d1bc1f10e1b4df08
akelcheg <akelcheg@gmail.com>
2016-06-11 13:56:30 +0300

    upd

---
+++ client/api/controllers/DescriptionController.js
@@ -0,0 +1,15 @@
+/**
+ * UsersController
+ *
+ * @description :: Server-side logic for managing users
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+
+  index: function (req, res) {
+    return res.view("description");
+  }
+
+};
+
--- client/config/routes.js
+++ client/config/routes.js
@@ -32,7 +32,8 @@ module.exports.routes = {
   *                                                                          *
   ***************************************************************************/
 
-  '/':  'IndexController.index'
+  '/':  'IndexController.index',
+  '/description':  'DescriptionController.index'
 
 
   /***************************************************************************
